5adbfdae4e92b9698355e6d908a8988e236f6908
dragonUbuntu <dragonfive2013@163.com>
2016-09-11 12:10:55 +0800

    修复取值范围的bug

--- poj3641.cpp
+++ poj3641.cpp
@@ -10,14 +10,14 @@
 using namespace std;
 // 找到那些p不是素数那是却满足条件的数
 // 判断一个数是不是素数
-bool is_prime(int num)
+bool is_prime(long long num)
 {
     if(num==2)
     {
         return false;
     }
     //int num_yueshu = 0;
-    for(int i=2;i*i<num;i++)
+    for(int i=2;i*i<=num;i++)
     {
         if(num%i==0)
         {
@@ -31,10 +31,10 @@ bool is_prime(int num)
 
 //判断一个数是否满足条件 快速幂运算 
 
-int quick_mod(int p,int a)
+long long quick_mod(int p,int a)
 {
-    int ret = 1;
-    int n = p;
+    long long ret = 1;
+    long long n = p;
     while(n>0)
     {
         if(n&1)
@@ -53,7 +53,7 @@ int main()
     fstream cin("test.txt");
 #endif
 
-    int p,a;
+    long long p,a;
     while(cin>>p>>a)
     {
         if(p==0 && a==0)
@@ -63,7 +63,7 @@ int main()
             cout<<"no"<<endl;
             continue;
         }
-        int ret = quick_mod(p,a);
+        long long ret = quick_mod(p,a);
         //cout<<ret<<" ";
         if(ret%p==a%p)
         {
